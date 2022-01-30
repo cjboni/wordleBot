f06dfbe42ccb770cd526f984a2af61a832a0a817
Halfbrit <56044490+Halfbrit@users.noreply.github.com>
2022-01-30 03:28:03 -0500

    Update rtTranscribe.py

--- rtTranscribe.py
+++ rtTranscribe.py
@@ -7,12 +7,14 @@ from pynput.keyboard import Key, Controller
 import time
 from configure import auth_key
 import boto3
+import os
 import playsound
 import pyaudio
  
 keyboard = Controller()
 
 buffer = []
+enteredWord = ""
 
 polly_client = boto3.Session(aws_access_key_id='AKIAXMKPYUNFS4LGCSEM', aws_secret_access_key='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', region_name='us-east-1').client('polly')
 
@@ -30,9 +32,20 @@ stream = p.open(
         input=True,
         frames_per_buffer=FRAMES_PER_BUFFER
 )   
- 
+
 # the AssemblyAI endpoint we're going to hit
 URL = "wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000&punctuate=False&format_text=False"
+
+introPhrase = 'Hi! Welcome to WordleBot'
+introResponse = polly_client.synthesize_speech(VoiceId='Joanna',
+                                        OutputFormat='mp3',
+                                        Text = introPhrase,
+                                        Engine = 'neural')
+intro = open('intro.mp3','wb')
+intro.write(introResponse['AudioStream'].read())
+intro.close()
+playsound.playsound('intro.mp3')
+os.remove('intro.mp3')
  
 async def send_receive():
 
@@ -86,6 +99,7 @@ async def send_receive():
                                                                         buffer.append(array[i]["text"].lower().translate(str.maketrans("", "", string.punctuation)))
                                                                 if buffer[-1] == "enter":
                                                                         keyboard.type(buffer[-2])
+                                                                        enteredWord = buffer[-2]
                                                                         mytext = buffer[-2]
                                                                         response = polly_client.synthesize_speech(VoiceId='Joanna',
                                                                                                                   OutputFormat='mp3',
@@ -95,9 +109,14 @@ async def send_receive():
                                                                         file.write(response['AudioStream'].read())
                                                                         file.close()
                                                                         playsound.playsound('speech.mp3')
+                                                                        os.remove('speech.mp3')
                                                                         buffer.clear()
                                                                 elif buffer[-1] == "stop":
                                                                         raise SystemExit
+                                                                elif buffer[-1] == "delete":
+                                                                        for i in range (len(enteredWord)):
+                                                                                keyboard.press(Key.backspace)
+                                                                                keyboard.release(Key.backspace)
                                 except websockets.exceptions.ConnectionClosedError as e:
                                         print(e)
                                         assert e.code == 4008
